f641ab686ef4960b3ffd6790447e6cc7c2b6952d
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-03-21 11:08:17 +0530

    Remove unused static file serving middleware

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 require("dotenv").config();
@@ -14,7 +13,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api/admin", adminRouter);
